b50456a963fa7f1c639ec2d714a49ee9b592659e
Rounak Chawla <ron.chawla9@gmail.com>
2021-08-23 21:07:43 +0530

    deploying frontend to production

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const app = express()
 
 app.use(express.json())
 app.use(cors())
+app.use(express.static('build'))
 morgan.token('body', (req, res) => {
   if (req.method === 'POST') {
     return JSON.stringify(req.body)
